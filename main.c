84180e2326731073067c6a55783bb5aeba01b9ea
Livhk <mehran.hosseinzadeh1@student.sharif.edu>
2016-12-13 14:29:54 -0800

    second edition

--- main.c
+++ main.c
@@ -1,5 +1,17 @@
 #include<stdio.h>
 
+int IsPrime(int n)
+{
+    int i = 0;
+    for(i =2; i * i <= a; i++)
+    {
+        if (n % i == 0)
+        {
+            return 0;
+        }
+    }
+    return 1;
+}
 int main()
 {
     int sum,a,b;
