c5c62cd4b424255e3b89502c47a4ff555701d03d
Mohamad <Mohamad>
2016-12-13 15:33:18 +0330

    salam

--- new1.c
+++ new1.c
@@ -2,13 +2,9 @@
 
 int main()
 {
-    printf("MOHAMAD Is A Genius!");
+    printf("MOHAMAD Is A Genius!\n");
 	int a,b,mul;
 	scanf("%d%d",&a,&b);
 	mul = a * b;
-<<<<<<< HEAD
 	printf("multiple is %d",mul);
-=======
-	printf("%d",mul);
->>>>>>> bd49085fa7d74f43d1b46edffe5d9bdbd747ed18
 }
